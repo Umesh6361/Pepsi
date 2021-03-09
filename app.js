34a051bfb43b2f610d897b13041b5d2ef96866dc
Umesh <kuwar51@gmail.com>
2021-03-09 18:07:35 +0530

    responsive

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-function imgslider(event){
- document.querySelector('.pepsi').src=event; 
+function imgslider(e){
+ document.querySelector('.pepsi').src=e; 
 }
 
 function changebg(color){
 const bg=document.querySelector('.sec');
 bg.style.background=color;
+}
+
+function togglemenu(){
+    const togglemenu=document.querySelector('.togglemenu');
+    togglemenu.classList.toggle('active')
+    const navigation=document.querySelector('.navigation');
+    navigation.classList.toggle('active')
+    
 }
